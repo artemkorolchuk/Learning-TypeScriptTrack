bd4002e76b6e2a45295f2bad8563b6cb4f234901
Artem Korolchuk <artemkorolchukua@gmail.com>
2026-06-13 01:18:42 +0200

    feat: implement space age convertor

---
+++ packages/easy/src/009-space-age/space-age.ts
@@ -0,0 +1,18 @@
+export const PLANETS = {
+	mercury: 0.2408467,
+	venus: 0.61519726,
+	earth: 1,
+	mars: 1.8808158,
+	jupiter: 11.862615,
+	saturn: 29.447498,
+	uranus: 84.016846,
+	neptune: 164.79132,
+} as const;
+
+export type Planet = keyof typeof PLANETS;
+const SECONDS_IN_EARTH_YEAR = 31_557_600;
+
+export function age(planet: Planet, seconds: number): number {
+  const result = seconds / SECONDS_IN_EARTH_YEAR / PLANETS[planet];
+  return Number(result.toFixed(2));
+}
