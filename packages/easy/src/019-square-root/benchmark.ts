f83fee88bf3434835a5803ca1051d2966a722222
Artem Korolchuk <artemkorolchukua@gmail.com>
2026-07-01 00:03:36 +0200

    test: add tests for squareRootBinary and squareRootNewton

--- packages/easy/src/019-square-root/benchmark.ts
+++ packages/easy/src/019-square-root/benchmark.ts
@@ -3,24 +3,26 @@ import { squareRootBinary, squareRootNewton } from "./square-root.js";
 const radicands = [10, 100, 1_000, 10_000, 100_000, 1_000_000];
 const AVERAGE_PARAM = 100_000;
 
-for (let n of radicands) {
-  const t0 = performance.now();
-  let iterationsBinary = 0;
-  for (let i = 0; i <= AVERAGE_PARAM; i++) {
-    const { iterations } = squareRootBinary(n);
-    iterationsBinary = iterations;
-  }
-  const t1 = performance.now();
-  const binaryTime = ((t1 - t0) / AVERAGE_PARAM).toFixed(6);
+for (const n of radicands) {
+	const t0 = performance.now();
+	let iterationsBinary = 0;
+	for (let i = 0; i <= AVERAGE_PARAM; i++) {
+		const { iterations } = squareRootBinary(n);
+		iterationsBinary = iterations;
+	}
+	const t1 = performance.now();
+	const binaryTime = ((t1 - t0) / AVERAGE_PARAM).toFixed(6);
 
-  const t2 = performance.now();
-  let iterationsNewton = 0
-  for (let i = 0; i <= AVERAGE_PARAM; i++) {
-    const { iterations } = squareRootNewton(n);
-    iterationsNewton = iterations;
-  }
-  const t3 = performance.now();
-  const newtonTime = ((t3 - t2) / AVERAGE_PARAM).toFixed(6);
+	const t2 = performance.now();
+	let iterationsNewton = 0;
+	for (let i = 0; i <= AVERAGE_PARAM; i++) {
+		const { iterations } = squareRootNewton(n);
+		iterationsNewton = iterations;
+	}
+	const t3 = performance.now();
+	const newtonTime = ((t3 - t2) / AVERAGE_PARAM).toFixed(6);
 
-  console.log(`n=${n} | binary: ${iterationsBinary} iters | newton: ${iterationsNewton} iters | binary ${binaryTime} ms | newton ${newtonTime} ms`);
+	console.log(
+		`n=${n} | binary: ${iterationsBinary} iters | newton: ${iterationsNewton} iters | binary ${binaryTime} ms | newton ${newtonTime} ms`,
+	);
 }
