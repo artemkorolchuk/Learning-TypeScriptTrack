71aeb82ac0701f060d459fee2c4f4299e4af989b
Artem Korolchuk <artemkorolchukua@gmail.com>
2026-06-30 23:48:54 +0200

    feat: implement newton method with benchmark measure

--- packages/easy/src/019-square-root/square-root.ts
+++ packages/easy/src/019-square-root/square-root.ts
@@ -19,3 +19,46 @@ export function squareRoot(radicand: number): number {
 
 	return low;
 }
+
+export function squareRootBinary(radicand: number): { result: number; iterations: number; } {
+	let low = 0;
+	let high = radicand;
+  let iterations = 0;
+  let result = 0;
+
+  while (low <= high) {
+    iterations += 1;
+		const mid = Math.floor((low + high) / 2);
+		const square = mid * mid;
+
+    if (square === radicand) {
+      result = mid;
+			return {result, iterations};
+		}
+
+		if (square <= radicand) {
+			low = mid + 1;
+		} else {
+			high = mid - 1;
+		}
+	}
+  result = low;
+	return {result, iterations};
+}
+
+
+export function squareRootNewton(n: number): { result: number; iterations: number; } {
+  let iterations = 0;
+  let result = n;
+  if (n < 2) return {result, iterations};                                // 0→0, 1→1
+  let guess = n;
+  let next = Math.floor((guess + n / guess) / 2);     // average guess & n/guess
+  while (next < guess) {                              // stop when it stops decreasing
+    guess = next;
+    next = Math.floor((guess + n / guess) / 2);
+    iterations += 1
+  }
+
+  result = guess;
+  return {result, iterations};
+}
